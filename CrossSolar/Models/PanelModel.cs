0c04c9e5fa2a74a3b96936d2aa5d30ca6c3861eb
Devin Andres Salemi <devinsalem@gmail.com>
2018-07-11 01:21:10 -0700

    Add files via upload

---
+++ CrossSolar/Models/PanelModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrossSolar.Models
+{
+    public class PanelModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Range(-90, 90)]
+        [RegularExpression(@"^-*?\d+\.\d{6}$")]
+        public double Latitude { get; set; }
+
+        [RegularExpression(@"^-*?\d+\.\d{6}$")]
+        [Range(-180, 180)] public double Longitude { get; set; }
+
+        [RegularExpression(@"^\w{16}$")]
+        [Required] public string Serial { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
---
+++ CrossSolar/Repository/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossSolar.Repository
+{
+    public interface IGenericRepository<T> 
+    {
+        Task<T> GetAsync(string id);
+
+        IQueryable<T> Query();
+
+        Task InsertAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
